75cfa84f680049c4d7abbd5ac684a097b95b47a9
jamal aw <jamal_aw_yonis@dell.com>
2020-11-22 21:24:40 -0500

    newely updated

---
+++ first_Course/models/user.go
@@ -0,0 +1,26 @@
+package models
+
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
+
+var (
+	users  *[]User
+	nextID = 1
+)
+
+func GetUsers() []*User {
+	return users
+
+}
+
+func AddUsers(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+
+	return u, nil
+
+}
